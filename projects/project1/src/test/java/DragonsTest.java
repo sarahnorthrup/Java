cb899da92a6913fd71017c706895658f9878e38f
Sarah <noir2000@gmail.com>
2022-03-17 09:28:55 -0500

    unit tests added

---
+++ projects/project1/src/test/java/DragonsTest.java
@@ -0,0 +1,22 @@
+import main.java.Dragons;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DragonsTest {
+
+    @BeforeEach
+    void setUp() {
+        Dragons dragons = new main.java.Dragons();
+    }
+
+    @AfterEach
+    void tearDown() {
+
+
+//
+//        assertEquals(expected:"You live to fight another day. Congrats Bro :)",dragons(), message:"did not work");
+    }
+}
---
+++ projects/project2/src/test/java/guessinggame/MymainTest.java
@@ -0,0 +1,22 @@
+package guessinggame;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MymainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
